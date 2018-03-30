003606c90b420bf2ff7e2b91e3f0c3c6bfa6da2a
htbkoo <h1017760@connect.hku.hk>
2018-03-30 17:48:34 +0800

    (WIP) making Cell immutable FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Cell.test.ts
+++ src/services/Cell.test.ts
@@ -1,39 +1,15 @@
 import Cell from './Cell';
 
 describe("Cell", function () {
-    it("should expose isLive=false by default", function () {
+    it("should expose cell.isLive=false for new Cell({isLive: false})", function () {
         // given
-        let cell = new Cell();
+        const expectedIsLive = false;
+        let cell = new Cell({isLive: expectedIsLive});
 
         // when
-        let isLive = cell.isLive();
+        let actualIsLive = cell.isLive();
 
         // then
-        expect(isLive).toEqual(false);
-    });
-
-    it("should return true for cell.isLive() after calling cell.born()", function () {
-        // given
-        let cell = new Cell();
-
-        // when
-        cell.born();
-        let isLive = cell.isLive();
-
-        // then
-        expect(isLive).toEqual(true);
-    });
-
-    it("should return false for cell.isLive() after calling cell.born() and then followed by cell.die()", function () {
-        // given
-        let cell = new Cell();
-
-        // when
-        cell.born();
-        cell.die();
-        let isLive = cell.isLive();
-
-        // then
-        expect(isLive).toEqual(false);
+        expect(actualIsLive).toEqual(expectedIsLive);
     });
 });
