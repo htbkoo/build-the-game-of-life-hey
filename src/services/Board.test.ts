31b63464afb2eb6a3a2d7a6c913113c9893e1aab
htbkoo <h1017760@connect.hku.hk>
2018-03-30 12:26:48 +0800

    parameterized Board.test.js FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Board.test.ts
+++ src/services/Board.test.ts
@@ -1,17 +1,20 @@
 import Board from './Board';
 
 describe("Board", function () {
-    it("should create a new board with width and height", function () {
-        // given
-        const width = 10, height = 20;
-        let board = new Board({width, height});
+    [
+        {width: 10, height: 20}
+    ].forEach(({width, height}) =>
+        it(`should create a new board with width=${width} and height=${height}`, function () {
+            // given
+            let board = new Board({width, height});
 
-        // when
-        let board_width = board.getWidth();
-        let board_height = board.getHeight();
+            // when
+            let board_width = board.getWidth();
+            let board_height = board.getHeight();
 
-        // then
-        expect(board_width).toEqual(width);
-        expect(board_height).toEqual(height);
-    });
+            // then
+            expect(board_width).toEqual(width);
+            expect(board_height).toEqual(height);
+        })
+    );
 });
