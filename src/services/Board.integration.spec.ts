bce295e0cafbb1137713bcd2c428d266f595242f
htbkoo <h1017760@connect.hku.hk>
2018-03-31 13:25:04 +0800

    added a full test case for board.getNumOfLivingNeighboursAt() in Board.integration.spec.ts FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Board.integration.spec.ts
+++ src/services/Board.integration.spec.ts
@@ -2,6 +2,7 @@ import * as path from "path";
 
 import Board from './Board';
 import boardBuilder from "./test-utils/boardBuilder";
+import * as fs from "fs";
 
 describe("Board (Integration)", function () {
     describe('getNumOfLivingNeighboursAt', function () {
@@ -24,11 +25,6 @@ describe("Board (Integration)", function () {
             {x: 1, y: 1, testCaseFileName: "board_neighbour_3x3_2.txt", expectedNum: 2},
             {x: 1, y: 1, testCaseFileName: "board_neighbour_3x3_8.txt", expectedNum: 8},
             {x: 1, y: 1, testCaseFileName: "board_neighbour_3x3_8b.txt", expectedNum: 8},
-
-            {x: 0, y: 0, testCaseFileName: "board_neighbour_4x3.txt", expectedNum: 3},
-            {x: 1, y: 0, testCaseFileName: "board_neighbour_4x3.txt", expectedNum: 0},
-            {x: 1, y: 1, testCaseFileName: "board_neighbour_4x3.txt", expectedNum: 0},
-            {x: 0, y: 0, testCaseFileName: "board_neighbour_4x3.txt", expectedNum: 3},
         ].forEach(({x, y, testCaseFileName, expectedNum}) =>
             it(`should, for "${testCaseFileName}", return ${expectedNum} for board.getNumOfLivingNeighboursAt({x: ${x}, y: ${y}})`, function () {
                 // given
@@ -39,7 +35,20 @@ describe("Board (Integration)", function () {
 
                 // then
                 expect(numOfLivingNeighbours).toEqual(expectedNum);
-
             }));
+
+        it("should compute all numOfLivingNeighboursAt correctly for board_neighbour_full case", function () {
+            // given
+            let board = boardBuilder.buildFromFile(path.normalize(`${__dirname}/test-resources/board/board_neighbour_full.txt`));
+            let expectedNumbers = fs.readFileSync(path.normalize(`${__dirname}/test-resources/board/board_neighbour_full_expected.txt`)).toString().split(/[\r\n]+/);
+
+            // when
+            let actualNumbers = new Array(board.getHeight()).fill(0).map((_, y) =>
+                new Array(board.getWidth()).fill(0).map((_, x) => board.getNumOfLivingNeighboursAt({x, y})).join("")
+            );
+
+            // then
+            expect(actualNumbers).toEqual(expectedNumbers);
+        });
     });
 });
