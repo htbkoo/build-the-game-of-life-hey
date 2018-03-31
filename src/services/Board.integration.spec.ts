ad91160bfc5e18cc922c74da5423631720cab92b
htbkoo <h1017760@connect.hku.hk>
2018-03-31 12:53:01 +0800

    added more test cases FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Board.integration.spec.ts
+++ src/services/Board.integration.spec.ts
@@ -24,7 +24,11 @@ describe("Board (Integration)", function () {
             {x: 1, y: 1, testCaseFileName: "board_neighbour_3x3_2.txt", expectedNum: 2},
             {x: 1, y: 1, testCaseFileName: "board_neighbour_3x3_8.txt", expectedNum: 8},
             {x: 1, y: 1, testCaseFileName: "board_neighbour_3x3_8b.txt", expectedNum: 8},
-            {x: 1, y: 1, testCaseFileName: "board_neighbour_4x3_0.txt", expectedNum: 0},
+
+            {x: 0, y: 0, testCaseFileName: "board_neighbour_4x3.txt", expectedNum: 3},
+            {x: 1, y: 0, testCaseFileName: "board_neighbour_4x3.txt", expectedNum: 0},
+            {x: 1, y: 1, testCaseFileName: "board_neighbour_4x3.txt", expectedNum: 0},
+            {x: 0, y: 0, testCaseFileName: "board_neighbour_4x3.txt", expectedNum: 3},
         ].forEach(({x, y, testCaseFileName, expectedNum}) =>
             it(`should, for "${testCaseFileName}", return ${expectedNum} for board.getNumOfLivingNeighboursAt({x: ${x}, y: ${y}})`, function () {
                 // given
