80c7f0a001ef6627a3b7fc12ded75bf95ecf9d6d
htbkoo <h1017760@connect.hku.hk>
2018-04-01 15:19:48 +0800

    added game.proceed() to use board.evolve() FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Game.test.ts
+++ src/services/Game.test.ts
@@ -1,4 +1,6 @@
 import Game from './Game';
+import * as sinon from "sinon";
+import Board from "./Board";
 
 describe("Game", function () {
     describe("constructor", function () {
@@ -16,17 +18,42 @@ describe("Game", function () {
     });
 
     describe("isLiveAt", function () {
-        // given
-        const width = 20, height = 30;
+        it("should expose isLiveAt", function () {
+            // given
+            const width = 20, height = 30;
 
-        // when
-        let game = new Game({width, height});
+            // when
+            let game = new Game({width, height});
 
-        // then
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                expect(game.isLiveAt({x, y})).toEqual(false);
+            // then
+            for (let y = 0; y < height; y++) {
+                for (let x = 0; x < width; x++) {
+                    expect(game.isLiveAt({x, y})).toEqual(false);
+                }
             }
-        }
+        });
+    });
+
+    describe("proceed", function () {
+        it("should set game._board to board.evolve() upon game.proceed()", function () {
+            // given
+            const mockNewBoard = Symbol("mockNewBoard");
+            // let game = sinon.createStubInstance(Game);
+            let game = new Game({width: 1, height: 1});
+            let mockBoardEvolve = jest.fn();
+            mockBoardEvolve.mockReturnValue(mockNewBoard);
+            // Object.defineProperty(game, "_board", {"evolve": mockBoardEvolve});
+            // game["_board"] = {"evolve": mockBoardEvolve};
+
+            let board = sinon.createStubInstance(Board);
+            board.evolve = sinon.stub().returns(mockNewBoard);
+            game["_board"] = board;
+
+            // when
+            game.proceed();
+
+            // then
+            expect(game["_board"]).toEqual(mockNewBoard);
+        });
     });
 });
