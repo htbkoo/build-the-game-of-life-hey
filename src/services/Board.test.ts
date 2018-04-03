b0a69b589f9bdd9e9cce60e7468f476f6592c8e9
htbkoo <h1017760@connect.hku.hk>
2018-04-04 07:56:31 +0800

    renamed test suite at Board.test.ts FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Board.test.ts
+++ src/services/Board.test.ts
@@ -1,70 +1,72 @@
 import Board from './Board';
 
 describe("Board", function () {
-    describe('Constructor', function () {
-        describe('Valid cases', function () {
-            [
-                {width: 10, height: 20},
-                {width: 20, height: 10},
-            ].forEach(({width, height}) =>
-                it(`should create a new board with width=${width} and height=${height}`, function () {
+    describe('factory methods', function () {
+        describe('newBlank', function () {
+            describe('Valid cases', function () {
+                [
+                    {width: 10, height: 20},
+                    {width: 20, height: 10},
+                ].forEach(({width, height}) =>
+                    it(`should create a new board with width=${width} and height=${height}`, function () {
+                        // given
+                        let board = Board.newBlank({width, height});
+
+                        // when
+                        let board_width = board.getWidth();
+                        let board_height = board.getHeight();
+
+                        // then
+                        expect(board_width).toEqual(width);
+                        expect(board_height).toEqual(height);
+                    })
+                );
+            });
+
+            describe('Error cases', function () {
+                it(`should throw Error when creating a new board with negative width`, function () {
                     // given
-                    let board = Board.newBlank({width, height});
+                    const negativeWidth = -10, height = 20;
 
                     // when
-                    let board_width = board.getWidth();
-                    let board_height = board.getHeight();
+                    let invalidBoardConstruction = () => Board.newBlank({width: negativeWidth, height});
 
                     // then
-                    expect(board_width).toEqual(width);
-                    expect(board_height).toEqual(height);
-                })
-            );
-        });
+                    expect(invalidBoardConstruction).toThrow(`Width (${negativeWidth}) must be be positive`);
+                });
 
-        describe('Error cases', function () {
-            it(`should throw Error when creating a new board with negative width`, function () {
-                // given
-                const negativeWidth = -10, height = 20;
+                it(`should throw Error when creating a new board with zero width`, function () {
+                    // given
+                    const zeroWidth = 0, height = 20;
 
-                // when
-                let invalidBoardConstruction = () => Board.newBlank({width: negativeWidth, height});
+                    // when
+                    let invalidBoardConstruction = () => Board.newBlank({width: zeroWidth, height});
 
-                // then
-                expect(invalidBoardConstruction).toThrow(`Width (${negativeWidth}) must be be positive`);
-            });
+                    // then
+                    expect(invalidBoardConstruction).toThrow(`Width (${zeroWidth}) must be be positive`);
+                });
 
-            it(`should throw Error when creating a new board with zero width`, function () {
-                // given
-                const zeroWidth = 0, height = 20;
+                it(`should throw Error when creating a new board with negative height`, function () {
+                    // given
+                    const width = 20, negativeHeight = -10;
 
-                // when
-                let invalidBoardConstruction = () => Board.newBlank({width: zeroWidth, height});
+                    // when
+                    let invalidBoardConstruction = () => Board.newBlank({width, height: negativeHeight});
 
-                // then
-                expect(invalidBoardConstruction).toThrow(`Width (${zeroWidth}) must be be positive`);
-            });
+                    // then
+                    expect(invalidBoardConstruction).toThrow(`Height (${negativeHeight}) must be be positive`);
+                });
 
-            it(`should throw Error when creating a new board with negative height`, function () {
-                // given
-                const width = 20, negativeHeight = -10;
+                it(`should throw Error when creating a new board with zero height`, function () {
+                    // given
+                    const width = 20, zeroHeight = 0;
 
-                // when
-                let invalidBoardConstruction = () => Board.newBlank({width, height: negativeHeight});
+                    // when
+                    let invalidBoardConstruction = () => Board.newBlank({width, height: zeroHeight});
 
-                // then
-                expect(invalidBoardConstruction).toThrow(`Height (${negativeHeight}) must be be positive`);
-            });
-
-            it(`should throw Error when creating a new board with zero height`, function () {
-                // given
-                const width = 20, zeroHeight = 0;
-
-                // when
-                let invalidBoardConstruction = () => Board.newBlank({width, height: zeroHeight});
-
-                // then
-                expect(invalidBoardConstruction).toThrow(`Height (${zeroHeight}) must be be positive`);
+                    // then
+                    expect(invalidBoardConstruction).toThrow(`Height (${zeroHeight}) must be be positive`);
+                });
             });
         });
     });
