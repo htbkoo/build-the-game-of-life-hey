9fc0353ba5b4b2815dfc8b246282aa3c917bb6d0
htbkoo <h1017760@connect.hku.hk>
2018-04-05 16:33:24 +0800

    parameterized true case for randomObjectGenerator.boolean FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/utils/randomObjectGenerator.test.ts
+++ src/services/utils/randomObjectGenerator.test.ts
@@ -5,17 +5,24 @@ import randomObjectGenerator from './randomObjectGenerator';
 
 describe("randomObjectGenerator", function () {
     describe('boolean', function () {
-        it('should return true if Math.random() returned >=0.5', sinonTest(function (this: sinon.SinonSandbox) {
-            // given
-            let anyNumberGreaterThanOrEqualToZeroPointFive = 0.5;
-            this.stub(Math, "random").returns(anyNumberGreaterThanOrEqualToZeroPointFive);
+        [
+            0.5,
+            0.51,
+            0.6,
+            0.9,
+            0.999
+        ].forEach(anyNumberGreaterThanOrEqualToZeroPointFive =>
+            it(`should return true if Math.random() returns ${anyNumberGreaterThanOrEqualToZeroPointFive} which is >=0.5`, sinonTest(function (this: sinon.SinonSandbox) {
+                // given
+                this.stub(Math, "random").returns(anyNumberGreaterThanOrEqualToZeroPointFive);
 
-            // when
-            let generatedBoolean = randomObjectGenerator.boolean();
+                // when
+                let generatedBoolean = randomObjectGenerator.boolean();
 
-            // then
-            expect(generatedBoolean).toEqual(true);
-        }));
+                // then
+                expect(generatedBoolean).toEqual(true);
+            }))
+        );
 
         it('should return false if Math.random() returned <0.5', sinonTest(function (this: sinon.SinonSandbox) {
             // given
