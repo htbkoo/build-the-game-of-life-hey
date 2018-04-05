e2b45960f503e6162d8c79a0194657a04c567af1
htbkoo <h1017760@connect.hku.hk>
2018-04-05 17:07:46 +0800

    added board.newRandomized() FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Board.test.ts
+++ src/services/Board.test.ts
@@ -1,5 +1,9 @@
+import {sinonTest} from "./test-utils/sinonWithTest";
+import * as sinon from "sinon";
+
 import Board, {Cells} from './Board';
 import Cell from "./Cell";
+import randomObjectGenerator from "./utils/randomObjectGenerator";
 
 describe("Board", function () {
     describe('factory methods', function () {
@@ -189,4 +193,36 @@ describe("Board", function () {
             expect(newBoard.isLiveAt({x: 1, y: 0})).toEqual(false);
         });
     });
+
+
+    describe("newRandomized", function () {
+        it("should get a new randomized board from board.newRandomized", sinonTest(function (this: sinon.SinonSandbox) {
+            // given
+            let stub = this.stub(randomObjectGenerator, "boolean");
+            stub
+                .onFirstCall().returns(false)
+                .onSecondCall().returns(true)
+                .onThirdCall().returns(false);
+            stub.returns(true);
+
+            // when
+            let board = Board.newBlank({width: 2, height: 3}).newRandomized();
+
+            // then
+            let expectedCells = [
+                [false, true],
+                [false, true],
+                [true, true],
+            ];
+
+            expectedCells.forEach((rows, y) =>
+                rows.forEach((expectedIsLive, x) =>
+                    // Unfortunately Jest does not support (a simple way for) customized assertion error message
+                    // Thus using the alternative suggested here: https://github.com/facebook/jest/issues/3293#issuecomment-361037383
+                    expect({x, y, isLive: board.isLiveAt({x, y})}).toEqual({x, y, isLive: expectedIsLive})
+                )
+            );
+        }));
+    });
+
 });
