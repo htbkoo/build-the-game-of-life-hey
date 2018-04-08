151380053db5065cc5d68dc00323a2d802b809e0
htbkoo <h1017760@connect.hku.hk>
2018-04-08 20:24:14 +0800

    fixed broken App.acceptance.spec.tsx FreeCodeCamp - Data Visualization Certification - React Projects

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,31 @@
 import * as React from 'react';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+// To avoid the "TS7016: Could not find a declaration file for module" error
+// reference: https://stackoverflow.com/a/42505940
+// const MuiThemeProvider = require('material-ui/styles/MuiThemeProvider');
+
 import Board from './BoardComponent';
 import ControlPanel from './ControlPanelComponent';
-import AppBar from 'material-ui/AppBar';
+
+import './App.css';
 
 class App extends React.Component {
     render() {
         return (
-            <div className="App">
-                <div className="App-Header">
-                    <AppBar title="Hey's Game of Life (ReactJs + TypeScript)" className="App-Header-AppBar" />
+            <MuiThemeProvider>
+                <div className="App">
+                    <div className="App-Header">
+                        <AppBar title="Hey's Game of Life (ReactJs + TypeScript)" className="App-Header-AppBar"/>
+                    </div>
+                    <div className="App-Body">
+                        <Board/>
+                        <ControlPanel/>
+                    </div>
+                    <div className="App-Footer">
+                    </div>
                 </div>
-                <div className="App-Body">
-                    <Board/>
-                    <ControlPanel/>
-                </div>
-                <div className="App-Footer">
-                </div>
-            </div>
+            </MuiThemeProvider>
         );
     }
 }
