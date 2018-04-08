e795f4e0e9a959fbbad7257b4f18e5b04ce85b42
htbkoo <h1017760@connect.hku.hk>
2018-04-08 20:10:00 +0800

    exposed game.getWidth() and game.getHeight() FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Game.ts
+++ src/services/Game.ts
@@ -3,14 +3,25 @@ import Board, {BoardCoordinates} from "./Board";
 export default class Game {
     private _board: Board;
 
+    // constructors
     constructor(dimension: { width: number; height: number }) {
         this._board = Board.newBlank(dimension);
     }
 
+    // getters
     isLiveAt(coors: BoardCoordinates): boolean {
         return this._board.isLiveAt(coors);
     }
 
+    getWidth(): number {
+        return this._board.getWidth();
+    }
+
+    getHeight(): number {
+        return this._board.getHeight();
+    }
+
+    // mutators
     toggleLiveAt(coors: BoardCoordinates) {
         let toggledIsLive = !this.isLiveAt(coors);
         this._board = this._board.withLiveAt({...coors, isLive: toggledIsLive});
