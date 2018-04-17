bd596756805fedda277271fafc9a6bf1f68e0e79
htbkoo <h1017760@connect.hku.hk>
2018-04-17 19:28:11 +0800

    cleaned up App.acceptance.spec.tsx FreeCodeCamp - Data Visualization Certification - React Projects

--- src/components/App.acceptance.spec.tsx
+++ src/components/App.acceptance.spec.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import {mount, shallow} from 'enzyme';
+import {mount} from 'enzyme';
 
 describe('App (Acceptance)', function () {
     it('renders without crashing', () => {
