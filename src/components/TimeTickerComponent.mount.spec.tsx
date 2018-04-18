8648eb63c6fd331d915bee24d44cc262b0f2f7b2
htbkoo <h1017760@connect.hku.hk>
2018-04-18 13:23:31 +0800

    updated <TimeTicker/> to trigger props.onTick upon regular interval FreeCodeCamp - Data Visualization Certification - React Projects

---
+++ src/components/TimeTickerComponent.mount.spec.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import {mount} from 'enzyme';
+
+import TimeTicker from './TimeTickerComponent';
+
+import {NO_OP} from './utils/common';
+
+describe('TimeTicker (mount)', function () {
+    xit('should, when ComponentWillMount, setInterval so that props.onTick() ', () => {
+        // given
+        jest.useFakeTimers();
+
+        const expectedInterval = 500, timeRightBeforeInterval = expectedInterval - 1;
+        let spyOnTick = jest.fn();
+
+        // when
+        let app = mount(<TimeTicker onTick={spyOnTick}/>);
+
+        // then
+        expect(spyOnTick.mock.calls.length).toEqual(0);
+
+        jest.runAllTimers();
+
+        expect(spyOnTick.mock.calls.length).toEqual(1);
+    });
+
+    it('should have state.intervalId to be defined after ComponentWillMount', () => {
+        // given
+        jest.useFakeTimers();
+
+        // when
+        let app = mount(<TimeTicker onTick={NO_OP}/>);
+
+        // then
+        expect(app.state('intervalId')).toBeDefined();
+    });
+
+    it('should, when ComponentWillMount, setInterval so that props.onTick() will happen per interval', () => {
+        // given
+        jest.useFakeTimers();
+
+        const expectedInterval = 500, residualInterval = 1,
+            timeRightBeforeInterval = expectedInterval - residualInterval;
+        let spyOnTick = jest.fn();
+
+        // when
+        let app = mount(<TimeTicker onTick={spyOnTick}/>);
+
+        // then
+        expect(spyOnTick.mock.calls.length).toEqual(0);
+
+        jest.advanceTimersByTime(timeRightBeforeInterval);
+
+        expect(spyOnTick.mock.calls.length).toEqual(0);
+
+        jest.advanceTimersByTime(residualInterval);
+
+        expect(spyOnTick.mock.calls.length).toEqual(1);
+    });
+});
--- src/components/TimeTickerComponent.tsx
+++ src/components/TimeTickerComponent.tsx
@@ -4,9 +4,37 @@ type TimeTickerProps = {
     onTick: () => void
 };
 
-const TimeTicker = (props: TimeTickerProps) => (
-    <div className="TimeTicker">
-    </div>
-);
+type TimeTickerState = {
+    intervalId: number
+};
+
+const INTERVAL = 500;
+
+class TimeTicker extends React.Component<TimeTickerProps, TimeTickerState> {
+
+    constructor(props: TimeTickerProps) {
+        super(props);
+
+        // Untested
+        this.state = {
+            intervalId: undefined
+        };
+    }
+
+    componentWillMount(): void {
+        // let intervalId = window.setInterval(this.props.onTick, INTERVAL);
+        let intervalId = window.setInterval(()=>{
+            console.log("triggered");
+            this.props.onTick();
+        }, INTERVAL);
+        this.setState({intervalId});
+    }
+
+    render() {
+        return (
+            <div/>
+        );
+    }
+}
 
 export default TimeTicker;
