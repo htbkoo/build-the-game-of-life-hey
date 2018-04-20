d4655bc6537feb6dd165bce50861f03833331909
htbkoo <h1017760@connect.hku.hk>
2018-04-20 19:47:10 +0800

    refactored Game.test.ts FreeCodeCamp - Data Visualization Certification - React Projects

--- src/services/Game.test.ts
+++ src/services/Game.test.ts
@@ -75,12 +75,7 @@ describe('Game', function () {
     describe('getNumGeneration', function () {
         it('should return 0 for game.getNumGeneration() when newly created', sinonTest(function (this: sinon.SinonSandbox) {
             // given
-            const mockBoard = sinon.createStubInstance(Board);
-
-            const someDimension = {width: 1, height: 1};
-            this.stub(Board, 'newBlank').withArgs(someDimension).returns(mockBoard);
-
-            let game = Game.new(someDimension);
+            const game = newGame.call(this);
 
             // when
             let numGeneration = game.getNumGeneration();
@@ -89,4 +84,16 @@ describe('Game', function () {
             expect(numGeneration).toEqual(0);
         }));
     });
+
+    const GAME_DEFAULTS = {
+        mockBoard: undefined,
+        dimension: {width: 1, height: 1}
+    };
+
+    function newGame(this: sinon.SinonSandbox, {mockBoard, dimension = GAME_DEFAULTS.dimension} = GAME_DEFAULTS) {
+        mockBoard = mockBoard ? mockBoard : sinon.createStubInstance(Board);
+        this.stub(Board, 'newBlank').withArgs(dimension).returns(mockBoard);
+
+        return Game.new(dimension);
+    }
 });
